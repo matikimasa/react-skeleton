017a9532a50220382a0894b9a34e8b07cd028760
Matti Koskinen <masa@MasaAir-2.local>
2016-04-13 18:33:53 +0300

    skeleton app

---
+++ src/components/List.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ListItem = require('./listItem.jsx');
+
+var ingredients = [{'id':1,'text':'salad'}, {'id':2,'text':'tofu'}, {'id':3,'text':'potatoes'}];
+
+var List = React.createClass({
+    render: function(){
+        var listItems = ingredients.map(function(item){
+            return <ListItem key={item.id} ingredient={item.text} />;
+        });
+
+        return (<ul>{listItems}</ul>);
+    }
+});
+
+module.exports = List;
